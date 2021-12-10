009fb655c1133ae3aa2fb12ba04004636cc75440
healplease <gavaaalex2012@gmail.com>
2021-12-10 11:32:33 +0200

    flask vars

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 import string
 import time
 
-from flask import Flask
+from flask import Flask, request, abort
 import markovify
 import telebot
 import dotenv
@@ -22,13 +22,13 @@ bot = telebot.TeleBot(app.config["TELEGRAM_BOT_TOKEN"])
 
 @app.route(f"/{app.config['TELEGRAM_BOT_TOKEN']}", methods=['POST'])
 def webhook():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_string = flask.request.get_data().decode('utf-8')
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
         update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
         return ''
     else:
-        flask.abort(403)
+        abort(403)
 
 @bot.message_handler(content_types=["text"])
 def text_handler(message: telebot.types.Message):
